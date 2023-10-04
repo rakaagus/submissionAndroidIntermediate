fac3922ea9ef6ba4f01fe4aedb06f03bbe9daa1e
ArtForYou <kakakunyukya@gmail.com>
2023-10-04 15:56:24 +0700

    Update Structure, add localization language, Add Package For Auth

---
+++ app/src/main/java/com/dicoding/submissionandroidintermediate/ui/SplashScreen/SplashScreen.kt
@@ -0,0 +1,12 @@
+package com.dicoding.submissionandroidintermediate.ui.SplashScreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dicoding.submissionandroidintermediate.R
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
